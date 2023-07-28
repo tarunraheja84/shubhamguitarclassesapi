2ece9182000e106d45e097c04a405b9070f81b40
Tarun <tarunraheja84@gmail.com>
2023-07-28 20:30:26 +0530

    next commit

--- src/routes/user/loginRoutes.js
+++ src/routes/user/loginRoutes.js
@@ -9,7 +9,7 @@ router.post('/login', async (req, res) => {
         try {
             const user = await users.findOne({phoneno:req.body.phoneno})
 
-        if(user.tokens.length!==1){
+        if(user.tokens.length!==0){
             res.status(405).send()
             return
         }
